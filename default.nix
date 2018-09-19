0b39f5421cd24604127af65e40eae60f4a130097
Peter Jones <pjones@devalot.com>
2018-09-19 11:02:04 -0700

    New build script and deployment script

--- default.nix
+++ default.nix
@@ -1,86 +1,34 @@
+{
+}@args:
+
 let
-  # All dependencies come from this package source:
-  pinned = {
-    owner  = "NixOS";
-    repo   = "nixpkgs-channels";
-
-    # NixOS 17.09 (stable) as of 1/30/2018:
-    rev    = "c2b668ee7264000b10d554754150ea9305b6ef3d";
-    sha256 = "1nqjsl163dahghidh06k3ynzq6kkmb9pzj1njw6ilfx7axygkyx8";
+  repo = fetchGit {
+    url = "git://git.devalot.com/edify.git";
+    rev = "440988b08fc27ea13119aa197e7a17344295f0e2";
   };
 
-  # Which version of Edify to use:
-  edifyRepo = {
-    url    = "git://git.devalot.com/edify.git";
-    rev    = "eed6ce977dbdd80cac90f73d4683810c75a62b8e";
-    sha256 = "1mdwr8r6w32vag17i8rrincd66ca1b8vy9kj3zz76yp9mh8sfsda";
-  };
-
-  # Load the host's nixpkgs, then the pinned version:
-  hostPkgs   = import <nixpkgs> {};
-  pinnedPkgs = import (hostPkgs.fetchFromGitHub pinned) {};
-  edifyPkg   = import (pinnedPkgs.fetchgit edifyRepo) { pkgs = pinnedPkgs; };
-
+  edify = import "${repo}/nix/builder.nix" args;
+  pkgs  = edify.pkgs;
 in
-{ pkgs ? pinnedPkgs
-}:
 
-pkgs.stdenv.mkDerivation rec {
+edify.mkDerivation rec {
   name = "rubyclass-${version}";
   version = "2.0";
-  src = builtins.fetchGit ./.;
+  src = ./.;
 
-  # Specifically don't want fixups for this package:
-  phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+  # Files to include in the zip archive:
+  extraFiles = [ "LICENSE" "README.md" "src" ];
 
   # Additional system dependencies:
-  buildInputs = with pkgs; [
-    inkscape # For SVG -> PDF
-    graphviz_2_32 # For DOT -> PDF
-    mscgen # For MSC -> PDF
-
-    # Markdown -> PDF:
-    edifyPkg
-    pandoc
-    haskellPackages.pandoc-citeproc
-    haskellPackages.pandoc-crossref
-
-    # TeX:
-    (texlive.combine {
-      inherit (texlive) scheme-small collection-binextra beamer
-        xcolor-solarized pgf standalone;
-    })
-
-    # For packaging:
-    zip
-
-    # For this course only:
+  buildInputs = [
     (pkgs.ruby_2_4.override {
       docSupport = true;
     })
   ];
 
-  buildPhase = ''
-    edify build --top "$(pwd)" courses/*.md
-  '';
-
+  # Link archive to generic name:
   installPhase = ''
-    dest=$out/${name}
-
-    mkdir -p $dest/handouts -p $dest/slides
-    cp -rp LICENSE README.md $dest/
-
-    # Copy PDF files into the correct locations:
-    find build -type f -name '*.handout.pdf' -exec cp '{}' $dest/handouts ';'
-    find build -type f -name '*.slides.pdf'  -exec cp '{}' $dest/slides ';'
-
-    # Rename PDF files:
-    for file in $dest/{handouts,slides}/*.pdf; do
-      mv $file $(echo $file | sed -E 's/[.](handout|slides)[.]pdf/.pdf/')
-    done
-
-    # Build archives:
-    ( cd $out && zip -9 -y -r -q ${name}.zip ${name} )
+    ( cd $out && ln -nfs ${name}.zip rubyclass.zip )
   '';
 
   # When installing gems, make them install locally.
---
+++ scripts/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+################################################################################
+set -e
+set -u
+
+################################################################################
+ssh_host="ursula.pmade.com"
+ssh_dir="/var/lib/websites/devalot.com/shared/files/rubyclass"
+
+################################################################################
+short_name=$(basename "$ssh_dir").zip
+link_file=result/$short_name
+
+################################################################################
+if [ ! -e "$link_file" ]; then
+  >&2 echo "run nix-build to generate $link_file"
+  exit
+fi
+
+################################################################################
+zip_file=$(realpath "$link_file")
+long_name=$(basename "$zip_file")
+
+################################################################################
+scp "$zip_file" "${ssh_host}:${ssh_dir}"
+ssh "$ssh_host" "cd $ssh_dir && ln -nfs $long_name $short_name"
